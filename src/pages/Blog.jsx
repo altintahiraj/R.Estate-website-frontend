10ca723348699a9829135fa336d6402bcc45b0e9
altintahiraj <altintahiraj357@gmail.com>
2026-04-13 00:03:26 +0200

    Create Blog page

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,12 @@
+const Blog = () => {
+  return (
+    <main className="page">
+      <section className="page__hero">
+        <h1>Blog</h1>
+        <p>Insights, updates, and stories from the IMOVA team.</p>
+      </section>
+    </main>
+  );
+};
+
+export default Blog;
